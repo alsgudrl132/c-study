082da00107de3454ba46722a14a8e4b902b2b9a9
alsgudrl132 <alsgudrl132@gmail.com>
2025-04-25 16:33:09 +0900

    2중 for

--- MyProject/loop.c
+++ MyProject/loop.c
@@ -30,9 +30,40 @@ int main(void)
   // }
 
   // do { } while (조건)
-  int i = 1;
-  do
+  // int i = 1;
+  // do
+  // {
+  //   printf("Hello World %d\n", i++);
+  // } while (i <= 10);
+
+  // 2 중 반복문
+  // for (int i = 1; i <= 3; i++)
+  // {
+  //   printf("첫번째 반복문 : %d\n", i);
+
+  //   for (int j = 1; j <= 5; j++)
+  //   {
+  //     printf("    두번째 반복문 : %d\n", j);
+  //   }
+  // }
+
+  // 구구단
+  // for (int i = 2; i <= 9; i++) 
+  // {
+  //   for (int j = 1; j <= 9; j++)
+  //   {
+  //     printf("%d x %d = %d\n", i, j, i * j);
+  //   }
+  // }
+
+  // 피라미드
+  for (int i = 0; i < 5; i++)
   {
-    printf("Hello World %d\n", i++);
-  } while (i <= 10);
+    for (int j = 0; j <= i; j++)
+    {
+      printf("*");
+    }
+      printf("\n");
+
+  }
 }
