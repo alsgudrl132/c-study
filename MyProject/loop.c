c6e96bb229bbec6ced8bd12ff3b9c1e0686e36e3
alsgudrl132 <alsgudrl132@gmail.com>
2025-04-25 16:17:07 +0900

    while

--- MyProject/loop.c
+++ MyProject/loop.c
@@ -17,7 +17,15 @@ int main(void)
   // printf("b 는 %d\n",b++); // 21
   // printf("b 는 %d\n",b); // 22
 
-  for (int i = 0; i <= 10; i++) {
-    printf("%d\n", i);
+  // for (int i = 0; i <= 10; i++) {
+  //   printf("%d\n", i);
+  // }
+
+  // while (조건) { }
+  int i = 1;
+  while (i <= 10)
+  {
+    printf("Hello World %d\n");
+    i++;
   }
 }
