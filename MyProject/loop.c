781294b42b92db0d7b1f31c49ea0afcf47aba7f0
alsgudrl132 <alsgudrl132@gmail.com>
2025-04-25 16:14:33 +0900

    for

---
+++ MyProject/loop.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  // printf("Hello World");
+  // return 0;
+
+  // int a = 10;
+  // printf("a 는 %d\n", a);
+  // a++;
+  // printf("a 는 %d\n", a);
+  // a++;
+  // printf("a 는 %d\n", a);
+
+  // int b = 20;
+  // printf("b 는 %d\n",++b); // 21
+  // printf("b 는 %d\n",b++); // 21
+  // printf("b 는 %d\n",b); // 22
+
+  for (int i = 0; i <= 10; i++) {
+    printf("%d\n", i);
+  }
+}
